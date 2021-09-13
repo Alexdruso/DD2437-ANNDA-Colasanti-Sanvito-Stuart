74e925e20e46d753567139b2720e0db05cdf7f98
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-09-13 18:16:26 +0200

    feat: add learning curve plotting

--- lab1/src/plotting.py
+++ lab1/src/plotting.py
@@ -21,25 +21,52 @@ def plot_decision_boundary(
 
     # Plot the models' decision boundaries (if any)
 
-    if models is not None:
-        for index in range(len(models)):
-            model = models[index]
-            name = names[index]
+    for index in range(len(models)):
+        model = models[index]
+        name = names[index]
 
-            coef = model.coef_.flatten()  # weights
-            w0 = model.intercept_  # bias
-            w1 = coef[0]
-            w2 = coef[1]
+        coef = model.coef_.flatten()  # weights
+        w0 = model.intercept_  # bias
+        w1 = coef[0]
+        w2 = coef[1]
 
-            step = 100
-            ds_x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), step)
-            # Compute x2 component given some x1:
-            # w^T x + x0 = 0 -> w0 + w1 * x1 + w2 * x2 = 0 -> x2 = - (w0 + w1*x1) / w2
-            ds_x2 = [-(w0 + w1 * x1) / w2 for x1 in ds_x1]
-            plt.plot(ds_x1, ds_x2, label=name)
+        step = 100
+        ds_x1 = np.linspace(X[:, 0].min(), X[:, 0].max(), step)
+        # Compute x2 component given some x1:
+        # w^T x + x0 = 0 -> w0 + w1 * x1 + w2 * x2 = 0 -> x2 = - (w0 + w1*x1) / w2
+        ds_x2 = [-(w0 + w1 * x1) / w2 for x1 in ds_x1]
+        plt.plot(ds_x1, ds_x2, label=name)
 
     plt.xlabel('x1')
     plt.ylabel('x2')
+    plt.grid()
+    plt.legend()
+
+    if path is not None:
+        plt.savefig(path)
+
+    plt.show()
+
+
+def plot_learning_curve(
+        errors: List,
+        names: List,
+        metric: str,
+        path: str = None
+) -> None:
+    if names is None:
+        names = []
+    plt.figure(figsize=(12, 7))
+
+    for index in range(len(errors)):
+        error = errors[index]
+        name = names[index]
+        ds_x1 = [i for i in range(len(error))]
+        ds_x2 = error
+        plt.plot(ds_x1, ds_x2, label=name)
+
+    plt.xlabel('Epoch')
+    plt.ylabel(metric)
     plt.grid()
     plt.legend()
 
