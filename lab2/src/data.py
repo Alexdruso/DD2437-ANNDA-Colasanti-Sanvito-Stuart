f26e7728947fffbbee81b22c18b954794e24dfff
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-09-18 20:18:27 +0200

    feat: implement data generation

---
+++ lab2/src/data.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.signal import square
+import pandas as pd
+
+output_functions = {
+    'sin': np.sin,
+    'square': square
+}
+
+
+def generate_periodic_function(
+        function: str = 'sin',
+        custom_function=None,
+        start: float = 0,
+        stop: float = 2 * np.pi,
+        step: float = 0.1,
+        reset_index: bool = True
+) -> pd.DataFrame:
+    x = np.arange(start=start, stop=stop, step=step)
+
+    output_function = output_functions[function] if function in output_functions else custom_function
+    y = output_function(2 * x)
+
+    df = pd.DataFrame(
+                {
+                    'x': x,
+                    'y': y
+                }
+            )
+
+    return df.reset_index(drop=True) if reset_index else df
