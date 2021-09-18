e495093fc7f6ce356db4498bae7bfecdfaee6010
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-09-18 20:31:50 +0200

    feat: implement plotting

---
+++ lab2/src/plotting.py
@@ -0,0 +1,45 @@
+from typing import List
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import matplotlib.cm as cm
+
+import pandas as pd
+
+
+def plot_2d_function(
+        dfs: List[pd.DataFrame],
+        function_names: List[str],
+        x: str = 'x',
+        y: str = 'y',
+        models: List = None,
+        names: List = None,
+        path: str = None
+) -> None:
+    if models is None: models = []
+    if names is None: names = []
+
+    plt.figure(figsize=(12, 7))
+
+    # Plot the functions to be approximated
+
+    for index in range(len(dfs)):
+        df = dfs[index]
+        plt.plot(df[x], df[y], label=function_names[index])
+
+    # Plot the models' decision boundaries (if any)
+
+    for index in range(len(models)):
+        model = models[index]
+        name = names[index]
+
+        plt.plot(x, model.predict(x), label=name)
+
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.grid()
+    plt.legend()
+
+    if path is not None:
+        plt.savefig(path)
+
+    plt.show()
