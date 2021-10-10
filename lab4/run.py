e8bb870f263a70510177b300d18c6a2a3268c42c
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-10-11 00:02:16 +0200

    refactor: change name

--- lab4/run.py
+++ lab4/run.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     ''' greedy layer-wise training '''
 
-    dbn.train_greedylayerwise(vis_trainset=train_imgs, lbl_trainset=train_lbls, n_iterations=10000)
+    dbn.train_greedylayerwise(vis_trainset=train_imgs, lbl_trainset=train_lbls, iterations_number=10000)
 
     dbn.recognize(train_imgs, train_lbls)
 
