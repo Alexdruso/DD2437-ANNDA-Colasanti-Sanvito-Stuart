1913f14072ffe489dfcb008105ae0094e75349ba
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-10-12 09:58:06 +0200

    fix: fix bias update

--- lab4/rbm.py
+++ lab4/rbm.py
@@ -137,9 +137,9 @@ class RestrictedBoltzmannMachine:
 
         # get the gradients from the arguments (replace the 0s below) and update the weight and bias parameters
 
-        self.delta_bias_v = self.learning_rate * (np.sum(v_0 - v_k, axis=0))
-        self.delta_bias_h = self.learning_rate * (np.sum(h_0 - h_k, axis=0))
         self.delta_weight_vh = self.learning_rate * ((v_0.T @ h_0) - (v_k.T @ h_k))
+        self.delta_bias_v = self.learning_rate * (v_0 - v_k)
+        self.delta_bias_h = self.learning_rate * (h_0 - h_k)
 
         self.weight_vh += self.delta_weight_vh
         self.bias_v += self.delta_bias_v
@@ -294,7 +294,7 @@ class RestrictedBoltzmannMachine:
         # find the gradients from the arguments (replace the 0s below) and update the weight and bias parameters.
 
         self.delta_weight_h_to_v = self.learning_rate * inputs.T @ (targets - predictions)
-        self.delta_bias_v = self.learning_rate * (np.sum(targets - predictions, axis=0))
+        self.delta_bias_v = self.learning_rate * (targets - predictions)
 
         self.weight_h_to_v += self.delta_weight_h_to_v
         self.bias_v += self.delta_bias_v
@@ -313,7 +313,7 @@ class RestrictedBoltzmannMachine:
         # find the gradients from the arguments (replace the 0s below) and update the weight and bias parameters.
 
         self.delta_weight_v_to_h = self.learning_rate * inputs.T @ (targets - predictions)
-        self.delta_bias_h = self.learning_rate * (np.sum(targets - predictions, axis=0))
+        self.delta_bias_h = self.learning_rate * (targets - predictions)
 
         self.weight_v_to_h += self.delta_weight_v_to_h
         self.bias_h += self.delta_bias_h
